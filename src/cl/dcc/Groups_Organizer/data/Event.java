24626eb518183a73324adba41a0aa15f29136c46
Ian Alon Yon <ian.alonyon@gmail.com>
2014-05-21 09:47:22 -0500

    Created Event, Group and Person classes
    Containers for the data with some constructors

--- src/cl/dcc/Groups_Organizer/data/Event.java
+++ src/cl/dcc/Groups_Organizer/data/Event.java
@@ -1,18 +1,40 @@
 package cl.dcc.Groups_Organizer.data;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by Ian on 14-04-2014.
  */
 public class Event {
-    public String name;
-    public String location;
-    public String confirmed;
-    public String invited;
+    public String name, description, location;
+    public Date datetime;
+    public ArrayList<Person> confirmed, guestList;
 
-    public Event(String name, String location, String confirmed, String invited) {
+    public Event(String name, String description, String location, Date datetime) {
         this.name = name;
+        this.description = description;
         this.location = location;
+        this.datetime = datetime;
+        this.confirmed = new ArrayList<Person>();
+        this.guestList = new ArrayList<Person>();
+    }
+
+    public Event(String name, String description, String location, Date datetime, ArrayList<Person> guestList) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.datetime = datetime;
+        this.confirmed = new ArrayList<Person>();
+        this.guestList = guestList;
+    }
+
+    public Event(String name, String description, String location, Date datetime, ArrayList<Person> confirmed, ArrayList<Person> guestList) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.datetime = datetime;
         this.confirmed = confirmed;
-        this.invited = invited;
+        this.guestList = guestList;
     }
 }
