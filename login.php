14fa82f79c4f95e8bfbf84218937607ea5a46c05
Gonzalo Infante <chalo.infante01@gmail.com>
2014-05-22 19:18:43 -0400

    Initial commit

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+require_once("internal/common_requires.php");
+require_once("internal/password_compat.php");
+
+header("Content-Type: text/plain");
+
+$validator = new Valitron\Validator($_POST);
+$validator->rule('required',['user', 'pass']);
+$validator->rule('lengthMin', array('user', 'pass'), 5);
+$validator->rule('lengthMax', 'user', 30);
+
+if(!$validator->validate()) {
+	$log->general("Invalid input in file login.php");
+	echo "INPUT VERIFICATION FAILED\n";
+	foreach($validator->errors() as $error) {
+		echo implode('\n', $error);
+	}
+	return;
+}
+
+$stmt = $conn->prepare("SELECT * FROM user WHERE user_name = ?");
+$stmt->bind_param('s', $_POST['user']);
+$stmt->execute();
+$row = $stmt->get_result()->fetch_assoc();
+$hashedPass = $row['pass'];
+if(password_verify($_POST['pass'],$hashedPass)){
+	echo "OK";
+} else {
+	die("LOGIN_FAILED");
+}
+session_start();
+$_SESSION['logged_username'] = $_POST['user'];
+//$now = new DateTime();
+//echo "time since last login ".($now->getTimestamp()-$_SESSION['logged_time'])."s";
+//$_SESSION['logged_time'] = $now->getTimestamp();
